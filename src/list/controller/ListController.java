f56e63989566ebbc517291801ac28ee56a0bbea2
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2017-10-05 13:18:15 -0600

    Changed the constructor parameters and added a data member, added topics to each kahoot

--- src/list/controller/ListController.java
+++ src/list/controller/ListController.java
@@ -26,20 +26,37 @@ public class ListController
 	}
 	private void showTheList()
 	{
+		String currentCreator = "";
 		for (int index = 0; index < myKahoots.size(); index += 1)
 		{
-			popup.displayText(myKahoots.get(index).toString());
+			currentCreator = myKahoots.get(index).getCreator();
 			
+			Kahoot currentKahoot = myKahoots.get(index);
+			String creator = currentKahoot.getCreator();
+			
+			popup.displayText(myKahoots.get(index).toString());
+			if (currentCreator.equals("nobody"))
+			{
+				for (int loop = 0; loop < 5; loop += 1)
+				{
+					popup.displayText("wow nodbody does a lot");
+				}
+			}
+			for (int currentLetterIndex = 0; currentLetterIndex < creator.length(); currentLetterIndex += 1)
+			{
+				popup.displayText(currentCreator.substring(currentLetterIndex, currentLetterIndex + 1));
+			}
 		}
+		
 	}
 	
 	private void fillTheList()
 	{
-		Kahoot fiftyStates = new Kahoot("Kashish", 50);
-		Kahoot mySecondKahoot = new Kahoot("Ethan", 2);
-		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
-		Kahoot animalColor = new Kahoot("Branton", 10);
-		Kahoot presidents = new Kahoot("Obama", 44);
+		Kahoot fiftyStates = new Kahoot("Kashish", 50, "The fifty United States");
+		Kahoot mySecondKahoot = new Kahoot("Ethan", 2, "The Double data type");
+		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE, "Everything-literally");
+		Kahoot animalColor = new Kahoot("Branton", 10, "All of the colors of animals");
+		Kahoot presidents = new Kahoot("Obama", 44, "The 44th president of the US");
 		
 		myKahoots.add(fiftyStates);
 		myKahoots.add(mySecondKahoot);
